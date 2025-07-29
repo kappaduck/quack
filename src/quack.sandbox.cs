4deba166344f7fbc22df7fefdd00ee8781d5673e
beauchama <beauchama@kappaduck.com>
2025-07-29 11:49:04 -0400

    Added Quack.Sandbox

---
+++ src/quack.sandbox.cs
@@ -0,0 +1,11 @@
+#!/usr/bin/env dotnet
+
+// Copyright (c) KappaDuck. All rights reserved.
+// The source code is licensed under MIT License.
+
+#:property TargetFramework=net10.0
+#:property GenerateDocumentationFile=false
+#:property IncludeNativeLibraries=true
+#:project KappaDuck.Quack
+
+Console.WriteLine("Hello, Quack!");
