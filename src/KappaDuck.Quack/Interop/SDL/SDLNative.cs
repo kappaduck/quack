4c2dd07fd1a58d2ccdbd5637ae7527fbee61ef59
beauchama <beauchama@kappaduck.com>
2025-07-29 20:10:20 -0400

    Added SDLNative (#149)

---
+++ src/KappaDuck.Quack/Interop/SDL/SDLNative.cs
@@ -0,0 +1,21 @@
+// Copyright (c) KappaDuck. All rights reserved.
+// The source code is licensed under MIT License.
+
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace KappaDuck.Quack.Interop.SDL;
+
+internal static partial class SDLNative
+{
+    internal const string Library = "SDL3";
+
+    internal static unsafe void Free<T>(T* pointer) where T : unmanaged => Free((nint)pointer);
+
+    internal static unsafe void Free<T>(T** pointer) where T : unmanaged => Free((nint)pointer);
+
+    internal static void Free(nint pointer) => SDL_free(pointer);
+
+    [LibraryImport(Library), UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    private static partial void SDL_free(nint pointer);
+}
