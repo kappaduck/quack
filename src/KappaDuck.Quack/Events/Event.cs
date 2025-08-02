4a8301086597ac990974b81ea79879ecb3c0b825
beauchama <beauchama@kappaduck.com>
2025-08-01 21:11:36 -0400

    Added Mouse events

--- src/KappaDuck.Quack/Events/Event.cs
+++ src/KappaDuck.Quack/Events/Event.cs
@@ -13,4 +13,34 @@ public struct Event
 {
     [FieldOffset(0)]
     private unsafe fixed byte _padding[128];
+
+    /// <summary>
+    /// The type of the event.
+    /// </summary>
+    [FieldOffset(0)]
+    public readonly EventType Type;
+
+    /// <summary>
+    /// The mouse button event data.
+    /// </summary>
+    [FieldOffset(0)]
+    public readonly MouseButtonEvent Mouse;
+
+    /// <summary>
+    /// The mouse device event data.
+    /// </summary>
+    [FieldOffset(0)]
+    public readonly MouseDeviceEvent MouseDevice;
+
+    /// <summary>
+    /// The mouse motion event data.
+    /// </summary>
+    [FieldOffset(0)]
+    public readonly MouseMotionEvent Motion;
+
+    /// <summary>
+    /// The mouse wheel event data.
+    /// </summary>
+    [FieldOffset(0)]
+    public readonly MouseWheelEvent Wheel;
 }
