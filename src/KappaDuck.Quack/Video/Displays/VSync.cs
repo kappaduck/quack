0b899350a3644e8674535a18cb3c5f10b1c0734e
beauchama <beauchama@kappaduck.com>
2025-08-03 12:07:18 -0400

    Added VSync (#188)

---
+++ src/KappaDuck.Quack/Video/Displays/VSync.cs
@@ -0,0 +1,20 @@
+// Copyright (c) KappaDuck. All rights reserved.
+// The source code is licensed under MIT License.
+
+namespace KappaDuck.Quack.Video.Displays;
+
+/// <summary>
+/// Constants values for vertical synchronization (VSync).
+/// </summary>
+public static class VSync
+{
+    /// <summary>
+    /// Disables vertical synchronization.
+    /// </summary>
+    public const int Disabled = 0;
+
+    /// <summary>
+    /// Enables adaptive vertical synchronization.
+    /// </summary>
+    public const int Adaptive = -1;
+}
