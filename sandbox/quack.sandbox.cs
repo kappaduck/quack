38250fdf76779036fa7839b8cd6d503bc440765b
beauchama <beauchama@kappaduck.com>
2025-08-02 21:08:38 -0400

    Added sandbox environment (#179)

---
+++ sandbox/quack.sandbox.cs
@@ -0,0 +1,28 @@
+#!/usr/bin/env dotnet
+
+// Copyright (c) KappaDuck. All rights reserved.
+// The source code is licensed under MIT License.
+
+#:property TargetFramework=net10.0
+#:package KappaDuck.Quack
+
+using KappaDuck.Quack;
+using KappaDuck.Quack.Core;
+using KappaDuck.Quack.Events;
+using KappaDuck.Quack.Windows;
+
+using QuackEngine _ = QuackEngine.Init(Subsystem.Video);
+
+using Window window = new("Quack! Sandbox", 800, 600, WindowState.Resizable);
+
+while (window.IsOpen)
+{
+    while (window.Poll(out Event e))
+    {
+        if (e.RequestQuit())
+        {
+            window.Close();
+            return;
+        }
+    }
+}
