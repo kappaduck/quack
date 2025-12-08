d996cadc8a91644f33772cf60d33d37a989b0ba0
beauchama <beauchama@kappaduck.com>
2025-12-08 16:44:50 -0500

    Added ApplicationTypeExtensions

---
+++ src/KappaDuck.Quack/Core/Extensions/ApplicationTypeExtensions.cs
@@ -0,0 +1,27 @@
+﻿using JetBrains.Annotations;
+
+namespace KappaDuck.Quack.Core.Extensions;
+
+/// <summary>
+/// Provides extension methods for <see cref="ApplicationType"/>.
+/// </summary>
+[PublicAPI]
+public static class ApplicationTypeExtensions
+{
+    extension(ApplicationType type)
+    {
+        /// <summary>
+        /// Gets the human-readable name of the application type.
+        /// </summary>
+        /// <remarks>
+        /// It returns "Unknown" for unrecognized application types.
+        /// </remarks>
+        public string Name => type switch
+        {
+            ApplicationType.Game => nameof(ApplicationType.Game),
+            ApplicationType.MediaPlayer => nameof(ApplicationType.MediaPlayer),
+            ApplicationType.Application => nameof(ApplicationType.Application),
+            _ => "Unknown"
+        };
+    }
+}
