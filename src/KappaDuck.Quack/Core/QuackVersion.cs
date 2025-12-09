6fb3ca9d708671c1d63f9ecef8b6f534f4d7c1d4
beauchama <beauchama@kappaduck.com>
2025-12-08 20:53:36 -0500

    Added QuackVersion

---
+++ src/KappaDuck.Quack/Core/QuackVersion.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) KappaDuck. All rights reserved.
+// The source code is licensed under MIT License.
+
+using KappaDuck.Quack.Interop.SDL;
+
+namespace KappaDuck.Quack.Core;
+
+/// <summary>
+/// Provides version information about Quack! and its components.
+/// </summary>
+[PublicAPI]
+public static class QuackVersion
+{
+    /// <summary>
+    /// Gets the current version of Quack!.
+    /// </summary>
+    public static Version Version { get; } = new(0, 3, 0);
+
+    /// <summary>
+    /// Gets the version of the SDL library used by Quack!.
+    /// </summary>
+    public static Version SDL { get; } = GetVersion(Native.SDL_GetVersion());
+
+    /// <summary>
+    /// Gets the version of the SDL_image library used by Quack!.
+    /// </summary>
+    public static Version Image { get; } = GetVersion(Native.IMG_Version());
+
+    /// <summary>
+    /// Gets the version of the SDL_ttf library used by Quack!.
+    /// </summary>
+    public static Version TTF { get; } = GetVersion(Native.TTF_Version());
+
+    private static Version GetVersion(int version)
+    {
+        int major = version / 1000000;
+        int minor = version / 1000 % 1000;
+        int patch = version % 1000;
+
+        return new Version(major, minor, patch);
+    }
+}
---
+++ src/KappaDuck.Quack/Interop/SDL/Native.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) KappaDuck. All rights reserved.
+// The source code is licensed under MIT License.
+
+namespace KappaDuck.Quack.Interop.SDL;
+
+internal static partial class Native
+{
+    private const string SDL = "SDL3";
+    private const string Image = "SDL3_image";
+    private const string TTF = "SDL3_ttf";
+}
