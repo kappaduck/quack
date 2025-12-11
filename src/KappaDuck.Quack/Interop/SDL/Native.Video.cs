0860b3e3fb0bcdcfe7b119f1dfc9bcbf07ced0aa
beauchama <beauchama@kappaduck.com>
2025-12-10 20:30:34 -0500

    Added RenderDriver and VideoDriver

---
+++ src/KappaDuck.Quack/Interop/SDL/Native.Video.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) KappaDuck. All rights reserved.
+// The source code is licensed under MIT License.
+
+using KappaDuck.Quack.Interop.SDL.Marshalling;
+
+namespace KappaDuck.Quack.Interop.SDL;
+
+internal static partial class Native
+{
+    [LibraryImport(SDL), UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [DefaultDllImportSearchPaths(DllImportSearchPath.SafeDirectories)]
+    [return: MarshalUsing(typeof(SDLOwnedStringMarshaller))]
+    internal static partial string SDL_GetCurrentVideoDriver();
+
+    [LibraryImport(SDL), UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [DefaultDllImportSearchPaths(DllImportSearchPath.SafeDirectories)]
+    internal static partial int SDL_GetNumRenderDrivers();
+
+    [LibraryImport(SDL), UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [DefaultDllImportSearchPaths(DllImportSearchPath.SafeDirectories)]
+    internal static partial int SDL_GetNumVideoDrivers();
+
+    [LibraryImport(SDL), UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [DefaultDllImportSearchPaths(DllImportSearchPath.SafeDirectories)]
+    [return: MarshalUsing(typeof(SDLOwnedStringMarshaller))]
+    internal static partial string SDL_GetRenderDriver(int index);
+
+    [LibraryImport(SDL), UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [DefaultDllImportSearchPaths(DllImportSearchPath.SafeDirectories)]
+    [return: MarshalUsing(typeof(SDLOwnedStringMarshaller))]
+    internal static partial string SDL_GetVideoDriver(int index);
+}
--- src/KappaDuck.Quack/Video/RenderDriver.cs
+++ src/KappaDuck.Quack/Video/RenderDriver.cs
@@ -1,4 +1,4 @@
-// Copyright (c) KappaDuck. All rights reserved.
+﻿// Copyright (c) KappaDuck. All rights reserved.
 // The source code is licensed under MIT License.
 
 using KappaDuck.Quack.Interop.SDL;
@@ -12,24 +12,32 @@ namespace KappaDuck.Quack.Video;
 /// A render driver is a set of code that handles rendering and texture management on a particular display.
 /// Normally there is only one, but some drivers may have several available with different capabilities.
 /// </remarks>
+[PublicAPI]
 public static class RenderDriver
 {
     /// <summary>
-    /// Gets the number of 2D rendering drivers available for the current display.
+    /// Gets the number of rendering drivers available for the current display.
     /// </summary>
-    public static int Count { get; } = SDL.Video.SDL_GetNumRenderDrivers();
+    public static int Count { get; } = Native.SDL_GetNumRenderDrivers();
 
     /// <summary>
-    /// Gets the name of a built-in 2D rendering driver.
+    /// Gets the name of the rendering driver at the specified index.
     /// </summary>
-    /// <param name="index">The index of a 2D rendering driver.</param>
-    /// <returns>The name of the 2D rendering driver with the given index.</returns>
-    public static string Get(int index) => SDL.Video.SDL_GetRenderDriver(index);
+    /// <param name="index">The index of the rendering driver to get the name of.</param>
+    /// <returns>The name of the rendering driver at the specified index.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="index"/> is negative or greater than or equal to <see cref="Count"/>.</exception>
+    public static string Get(int index)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(index);
+        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(index, Count);
+
+        return Native.SDL_GetRenderDriver(index);
+    }
 
     /// <summary>
-    /// Get all available 2D rendering drivers.
+    /// Get all available rendering drivers.
     /// </summary>
-    /// <returns>The names of all available 2D rendering drivers.</returns>
+    /// <returns>The names of all available rendering drivers.</returns>
     public static string[] GetAll()
     {
         string[] drivers = new string[Count];
