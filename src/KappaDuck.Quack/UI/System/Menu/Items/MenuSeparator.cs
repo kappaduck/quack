aa4ff288b933c27320c9b1f1ea2e45661c054d0e
beauchama <beauchama@kappaduck.com>
2026-01-28 22:12:12 -0500

    starting major overhaul for 0.5.0

--- src/KappaDuck.Quack/UI/System/Menu/Items/MenuSeparator.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) KappaDuck. All rights reserved.
-// The source code is licensed under MIT License.
-
-namespace KappaDuck.Quack.UI.System.Menu.Items;
-
-/// <summary>
-/// Represents a visual separator within a menu, used to group related menu items and improve menu organization.
-/// </summary>
-public sealed record MenuSeparator : IMenuItem;
