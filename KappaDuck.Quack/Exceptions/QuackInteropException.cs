dc33018efb6561df9b995c11f6edcc26ee916087
beauchama <beauchama@kappaduck.com>
2026-02-02 10:40:43 -0500

    Added Exceptions

---
+++ KappaDuck.Quack/Exceptions/QuackInteropException.cs
@@ -0,0 +1,32 @@
+// Copyright (c) KappaDuck. All rights reserved.
+// The source code is licensed under MIT License.
+
+namespace KappaDuck.Quack.Exceptions;
+
+/// <summary>
+/// An exception that is thrown when an interop call fails in Quack!.
+/// </summary>
+public sealed class QuackInteropException : QuackException
+{
+    private QuackInteropException(string message) : base(message)
+    {
+    }
+
+    internal static void ThrowIf([DoesNotReturnIf(true)] bool condition, [CallerMemberName] string memberName = "")
+    {
+        if (condition)
+            Throw(memberName);
+    }
+
+    internal static void ThrowIfFailed(bool success, [CallerMemberName] string memberName = "")
+        => ThrowIf(!success, memberName);
+
+    [DoesNotReturn]
+    private static void Throw(string memberName)
+    {
+        string error = SDL3.GetError();
+        SDL3.ClearError();
+
+        throw new QuackInteropException($"Interop call failed in [{memberName}]: {error}");
+    }
+}
