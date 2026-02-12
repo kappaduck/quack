c9cd2e852c953bb33b568de98503c030f2ddce8b
beauchama <beauchama@kappaduck.com>
2026-02-12 09:01:42 -0500

    Added SystemPreferences

--- KappaDuck.Quack/Interop/SDL/SDL3.System.cs
+++ KappaDuck.Quack/Interop/SDL/SDL3.System.cs
@@ -1,12 +1,28 @@
 // Copyright (c) KappaDuck. All rights reserved.
 // The source code is licensed under MIT License.
 
+using KappaDuck.Quack.System;
+
 namespace KappaDuck.Quack.Interop.SDL;
 
 internal static partial class SDL3
 {
     internal static partial class System
     {
+        [LibraryImport(nameof(SDL3), EntryPoint = "SDL_DisableScreenSaver"), UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        [DefaultDllImportSearchPaths(DllImportSearchPath.SafeDirectories)]
+        [return: MarshalAs(UnmanagedType.U1)]
+        internal static partial bool DisableScreenSaver();
+
+        [LibraryImport(nameof(SDL3), EntryPoint = "SDL_EnableScreenSaver"), UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        [DefaultDllImportSearchPaths(DllImportSearchPath.SafeDirectories)]
+        [return: MarshalAs(UnmanagedType.U1)]
+        internal static partial bool EnableScreenSaver();
+
+        [LibraryImport(nameof(SDL3), EntryPoint = "SDL_GetSystemTheme"), UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        [DefaultDllImportSearchPaths(DllImportSearchPath.SafeDirectories)]
+        internal static partial Theme GetSystemTheme();
+
         [LibraryImport(nameof(SDL3), EntryPoint = "SDL_OpenURL", StringMarshalling = StringMarshalling.Utf8), UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
         [DefaultDllImportSearchPaths(DllImportSearchPath.SafeDirectories)]
         [return: MarshalAs(UnmanagedType.U1)]
