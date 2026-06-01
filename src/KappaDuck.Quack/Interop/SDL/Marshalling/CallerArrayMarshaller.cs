af671b9c5ccd5906cbe3a25757af55d6bf94a4ed
beauchama <beauchama@kappaduck.com>
2026-06-01 09:37:52 -0400

    Added custom marshallers (#531)

---
+++ src/KappaDuck.Quack/Interop/SDL/Marshalling/CallerArrayMarshaller.cs
@@ -0,0 +1,27 @@
+// Copyright (c) KappaDuck.
+// Licensed under the MIT license.
+
+namespace KappaDuck.Quack.Interop.SDL.Marshalling;
+
+/// <summary>
+/// Custom marshaller for a pointer to an array owned by the caller.
+/// </summary>
+/// <remarks>
+/// It will free the allocated array.
+/// </remarks>
+/// <typeparam name="T">The managed element type</typeparam>
+/// <typeparam name="TUnmanaged">The unmanaged element type</typeparam>
+[ContiguousCollectionMarshaller]
+[CustomMarshaller(typeof(Span<>), MarshalMode.ManagedToUnmanagedOut, typeof(CallerArrayMarshaller<,>))]
+internal static unsafe class CallerArrayMarshaller<T, TUnmanaged> where TUnmanaged : unmanaged
+{
+    internal static Span<T> AllocateContainerForManagedElements(TUnmanaged* unmanaged, int length)
+        => unmanaged is null ? [] : new T[length];
+
+    internal static ReadOnlySpan<TUnmanaged> GetUnmanagedValuesSource(TUnmanaged* unmanaged, int length)
+        => unmanaged is null ? [] : new ReadOnlySpan<TUnmanaged>(unmanaged, length);
+
+    internal static Span<T> GetManagedValuesDestination(Span<T> managed) => managed;
+
+    internal static void Free(TUnmanaged* unmanaged) => SDL3.Free(unmanaged);
+}
---
+++ src/KappaDuck.Quack/Interop/SDL/Marshalling/CallerStringMarshaller.cs
@@ -0,0 +1,18 @@
+// Copyright (c) KappaDuck.
+// Licensed under the MIT license.
+
+namespace KappaDuck.Quack.Interop.SDL.Marshalling;
+
+/// <summary>
+/// Custom marshaller for a caller-owned UTF-8 encoded string.
+/// </summary>
+/// <remarks>
+/// It will free the allocated UTF-8 encoded string.
+/// </remarks>
+[CustomMarshaller(typeof(string), MarshalMode.ManagedToUnmanagedOut, typeof(CallerStringMarshaller))]
+internal static unsafe class CallerStringMarshaller
+{
+    internal static string? ConvertToManaged(byte* unmanaged) => Marshal.PtrToStringUTF8(unmanaged);
+
+    internal static void Free(byte* unmanaged) => SDL3.Free(unmanaged);
+}
