85c244fe392c1c991725f8e47cfc8428a36a9461
beauchama <beauchama@kappaduck.com>
2026-06-03 22:29:13 -0400

    Added Properties (#538)

---
+++ src/KappaDuck.Quack/Interop/SDL/Properties.cs
@@ -0,0 +1,57 @@
+// Copyright (c) KappaDuck.
+// Licensed under the MIT license.
+
+using KappaDuck.Quack.Exceptions;
+
+namespace KappaDuck.Quack.Interop.SDL;
+
+internal sealed class Properties : IDisposable
+{
+    private readonly uint _id;
+
+    internal Properties()
+    {
+        _id = SDL3.CreateProperties();
+        SDLThrowHelper.ThrowIfZero(0, nameof(SDL3.CreateProperties));
+    }
+
+    internal Properties(Properties properties) : this()
+        => SDLThrowHelper.ThrowIfFailed(SDL3.CopyProperties(properties, _id), nameof(SDL3.CopyProperties));
+
+    public static implicit operator uint(Properties properties) => properties._id;
+
+    public void Dispose() => SDL3.DestroyProperties(_id);
+
+    internal static bool Get(uint propertiesId, string name, bool defaultValue) => SDL3.GetBooleanProperty(propertiesId, name, defaultValue);
+
+    internal static float Get(uint propertiesId, string name, float defaultValue) => SDL3.GetFloatProperty(propertiesId, name, defaultValue);
+
+    internal static int Get(uint propertiesId, string name, int defaultValue)
+    {
+        long value = SDL3.GetNumberProperty(propertiesId, name, defaultValue);
+        return int.CreateChecked(value);
+    }
+
+    internal static T Get<T>(uint propertiesId, string name, T defaultValue) where T : struct, Enum
+    {
+        object obj = Convert.ChangeType(defaultValue, defaultValue.GetTypeCode());
+
+        long value = SDL3.GetNumberProperty(propertiesId, name, Convert.ToInt64(obj));
+        return Enum.Parse<T>(value.ToString().AsSpan());
+    }
+
+    internal static unsafe nint Get(uint propertiesId, string name, void* defaultValue) => SDL3.GetPointerProperty(propertiesId, name, defaultValue);
+
+    internal static string Get(uint propertiesId, string name, string defaultValue) => SDL3.GetStringProperty(propertiesId, name, defaultValue);
+
+    internal void Set(string name, bool value) => SDLThrowHelper.ThrowIfFailed(SDL3.SetBooleanProperty(_id, name, value));
+
+    internal void Set(string name, float value) => SDLThrowHelper.ThrowIfFailed(SDL3.SetFloatProperty(_id, name, value));
+
+    internal void Set(string name, int value) => SDLThrowHelper.ThrowIfFailed(SDL3.SetNumberProperty(_id, name, value));
+
+    internal unsafe void Set<T>(string name, T* value) where T : unmanaged
+        => SDLThrowHelper.ThrowIfFailed(SDL3.SetPointerProperty(_id, name, value));
+
+    internal void Set(string name, string value) => SDLThrowHelper.ThrowIfFailed(SDL3.SetStringProperty(_id, name, value));
+}
---
+++ src/KappaDuck.Quack/Interop/SDL/SDL3.Properties.cs
@@ -0,0 +1,74 @@
+// Copyright (c) KappaDuck.
+// Licensed under the MIT license.
+
+using KappaDuck.Quack.Interop.SDL.Marshalling;
+
+namespace KappaDuck.Quack.Interop.SDL;
+
+internal static partial class SDL3
+{
+    [LibraryImport(nameof(SDL3), EntryPoint = "SDL_CopyProperties")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [return: MarshalAs(UnmanagedType.I1)]
+    internal static partial bool CopyProperties(uint source, uint destination);
+
+    [LibraryImport(nameof(SDL3), EntryPoint = "SDL_CreateProperties")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial uint CreateProperties();
+
+    [LibraryImport(nameof(SDL3), EntryPoint = "SDL_DestroyProperties")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial void DestroyProperties(uint propertiesId);
+
+    [LibraryImport(nameof(SDL3), EntryPoint = "SDL_GetBooleanProperty", StringMarshalling = StringMarshalling.Utf8)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [return: MarshalAs(UnmanagedType.I1)]
+    internal static partial bool GetBooleanProperty(uint propertiesId, string name, [MarshalAs(UnmanagedType.I1)] bool defaultValue);
+
+    [LibraryImport(nameof(SDL3), EntryPoint = "SDL_GetFloatProperty", StringMarshalling = StringMarshalling.Utf8)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial float GetFloatProperty(uint propertiesId, string name, float defaultValue);
+
+    [LibraryImport(nameof(SDL3), EntryPoint = "SDL_GetNumberProperty", StringMarshalling = StringMarshalling.Utf8)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial long GetNumberProperty(uint propertiesId, string name, long defaultValue);
+
+    [LibraryImport(nameof(SDL3), EntryPoint = "SDL_GetPointerProperty", StringMarshalling = StringMarshalling.Utf8)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static unsafe partial nint GetPointerProperty(uint propertiesId, string name, void* defaultValue);
+
+    [LibraryImport(nameof(SDL3), EntryPoint = "SDL_GetStringProperty", StringMarshalling = StringMarshalling.Utf8)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [return: MarshalUsing(typeof(SDLStringMarshaller))]
+    internal static partial string GetStringProperty(uint propertiesId, string name, string defaultValue);
+
+    [LibraryImport(nameof(SDL3), EntryPoint = "SDL_SetAppMetadataProperty", StringMarshalling = StringMarshalling.Utf8)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [return: MarshalAs(UnmanagedType.I1)]
+    internal static partial bool SetAppMetadataProperty(string name, string value);
+
+    [LibraryImport(nameof(SDL3), EntryPoint = "SDL_SetBooleanProperty", StringMarshalling = StringMarshalling.Utf8)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [return: MarshalAs(UnmanagedType.I1)]
+    internal static partial bool SetBooleanProperty(uint propertiesId, string name, [MarshalAs(UnmanagedType.I1)] bool value);
+
+    [LibraryImport(nameof(SDL3), EntryPoint = "SDL_SetFloatProperty", StringMarshalling = StringMarshalling.Utf8)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [return: MarshalAs(UnmanagedType.I1)]
+    internal static partial bool SetFloatProperty(uint propertiesId, string name, float value);
+
+    [LibraryImport(nameof(SDL3), EntryPoint = "SDL_SetNumberProperty", StringMarshalling = StringMarshalling.Utf8)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [return: MarshalAs(UnmanagedType.I1)]
+    internal static partial bool SetNumberProperty(uint propertiesId, string name, long value);
+
+    [LibraryImport(nameof(SDL3), EntryPoint = "SDL_SetPointerProperty", StringMarshalling = StringMarshalling.Utf8)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [return: MarshalAs(UnmanagedType.I1)]
+    internal static unsafe partial bool SetPointerProperty(uint propertiesId, string name, void* value);
+
+    [LibraryImport(nameof(SDL3), EntryPoint = "SDL_SetStringProperty", StringMarshalling = StringMarshalling.Utf8)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [return: MarshalAs(UnmanagedType.I1)]
+    internal static partial bool SetStringProperty(uint propertiesId, string name, string value);
+}
