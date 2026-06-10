8fc7b29d7dd46e76d19699e65b173398330e7794
beauchama <beauchama@kappaduck.com>
2026-06-10 14:14:19 -0400

    Added AsyncIndeterminateProgressReporter

---
+++ tests/Unit.Tests/Progress/AsyncIndeterminateProgressReporterTests.cs
@@ -0,0 +1,47 @@
+// Copyright (c) KappaDuck.
+// Licensed under the MIT license.
+
+using KappaDuck.Quack.Progress;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Unit.Tests.Progress;
+
+internal sealed class AsyncIndeterminateProgressReporterTests : IDisposable
+{
+    private readonly AsyncIndeterminateProgressReporter _reporter = new();
+
+    public void Dispose() => _reporter.Dispose();
+
+    [Test]
+    [SuppressMessage("Performance", "CA1849:Call async methods when in an async method", Justification = "The test requires to test the method Cancel")]
+    public async Task CancelShouldRequestToCancelTheToken()
+    {
+        _reporter.Cancel();
+        await _reporter.CancellationToken.IsCancellationRequested.Should().BeTrue();
+    }
+
+    [Test]
+    public async Task CancelAfterShouldRequestToCancelTheToken()
+    {
+        _reporter.CancelAfter(TimeSpan.FromMilliseconds(1));
+        await Task.Delay(1);
+
+        await _reporter.CancellationToken.IsCancellationRequested.Should().BeTrue();
+    }
+
+    [Test]
+    public async Task CancelAfterByMilliSecondsShouldRequestToCancelTheToken()
+    {
+        _reporter.CancelAfter(1);
+        await Task.Delay(1);
+
+        await _reporter.CancellationToken.IsCancellationRequested.Should().BeTrue();
+    }
+
+    [Test]
+    public async Task CancelAsyncShouldRequestToCancelTheToken()
+    {
+        await _reporter.CancelAsync();
+        await _reporter.CancellationToken.IsCancellationRequested.Should().BeTrue();
+    }
+}
