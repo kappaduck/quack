910a00b13a10d94dd5e7ab97598db80dca538ea3
beauchama <beauchama@kappaduck.com>
2026-06-22 21:27:19 -0400

    Added VSync and LogicalPresentation

---
+++ src/KappaDuck.Quack/Video/VSync.cs
@@ -0,0 +1,20 @@
+// Copyright (c) KappaDuck.
+// Licensed under the MIT license.
+
+namespace KappaDuck.Quack.Video;
+
+/// <summary>
+/// Constants values for vertical synchronization (VSync) settings.
+/// </summary>
+public static class VSync
+{
+    /// <summary>
+    /// Disable the vertical synchronization.
+    /// </summary>
+    public const int Disabled = 0;
+
+    /// <summary>
+    /// Enable the adaptive vertical synchronization.
+    /// </summary>
+    public const int Adaptive = -1;
+}
